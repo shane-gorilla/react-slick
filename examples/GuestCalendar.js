6f6328893b08edf384202e1c35107bb316773992
Shane McDermed <shane.mcdermed@gorillalogic.com>
2016-10-27 15:34:32 +0100

    NBCRESP-2220 - guest list functionality to match spec

--- examples/GuestCalendar.js
+++ examples/GuestCalendar.js
@@ -3,25 +3,26 @@ import Slider from '../src/slider'
 
 export default class GuestCalendar extends Component {
   componentDidMount() {
-    //   requestAnimationFrame(() => this.refs.slider.slickNext())
+      requestAnimationFrame(() => this.refs.slider.slickNext())
   }
   render() {
     const settings = {
       dots: true,
-      infinite: true,
+      infinite: false,
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
       variableWidth: true,
-      dots: false
+      dots: false,
+      swipeToSlide: true,
+      initialSlide: 1 // The slide index of the previous day. 
+                      // Will slide to current day when `componentDidMount`
     };
     return (
       <div>
         <h2>Guest Calendar</h2>
         <Slider ref="slider" {...settings}>
           <div className="day">
-            <h3 className="guest">1</h3>
-            <h3 className="guest">1</h3>
             <h3 className="guest">1</h3>
          </div>
           <div className="day">
@@ -30,13 +31,19 @@ export default class GuestCalendar extends Component {
          </div>
           <div className="day">
             <h3 className="guest">3</h3>
+            <h3 className="guest">3</h3>
+            <h3 className="guest">3</h3>
          </div>
           <div className="day">
             <h3 className="guest">4</h3>
             <h3 className="guest">4</h3>
             <h3 className="guest">4</h3>
+            <h3 className="guest">4</h3>
          </div>
           <div className="day">
+            <h3 className="guest">5</h3>
+            <h3 className="guest">5</h3>
+            <h3 className="guest">5</h3>
             <h3 className="guest">5</h3>
             <h3 className="guest">5</h3>
          </div>
